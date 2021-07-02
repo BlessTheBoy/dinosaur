293d5ae266180c65e9b0bcad52c855f79c0a64f5
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-07-02 15:32:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/api/dinoname", async (req, res) => {
+  var options = {
+    method: "GET",
+    url: "https://alexnormand-dino-ipsum.p.rapidapi.com/",
+    params: { paragraphs: "1", words: "2", format: "json" },
+    headers: {
+      "x-rapidapi-key": "f43897b1b3msh44fd0bd3f56e039p143928jsn180068690a71",
+      "x-rapidapi-host": "alexnormand-dino-ipsum.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+// server route for getting dinoImage
+app.get("/api/dinoimage", (req, res) => {
+  const { q } = req.query;
+  var options = {
+    method: "GET",
+    url: "https://bing-image-search1.p.rapidapi.com/images/search",
+    params: { q: q + " dinosaur", count: "10" },
+    headers: {
+      "x-rapidapi-key": "f43897b1b3msh44fd0bd3f56e039p143928jsn180068690a71",
+      "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      let images = response.data.value;
+      let resImage = images[Math.floor(Math.random() * images.length)];
+      res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      });
+      res.status(200).json(resImage);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,51 @@
+console.log("Script.js loaded");
+const btnLoad = document.querySelector("#btnLoad");
+btnLoad.addEventListener("click", generateResults);
+const container = document.getElementById("container");
+generateResults();
+
+async function generateResults() {
+  const name = await getDinoName();
+
+  const dinoName = name[0].join(" ");
+  const dinoNameSlug = name[0][0];
+  const dinoImage = await getDinoImage(dinoNameSlug);
+
+  console.log(dinoName);
+  console.log(dinoImage);
+
+  let head = document.querySelector("#dinoHead")
+    ? document.querySelector("#dinoHead")
+    : document.createElement("p");
+  head.innerHTML = dinoName;
+  head.id = "dinoHead";
+  head.className = "result-head";
+
+  let image = document.querySelector("#dinoImage")
+    ? document.querySelector("#dinoImage")
+    : document.createElement("img");
+  image.src = dinoImage;
+  image.id = "dinoImage";
+  image.alt = dinoName;
+  image.className = "result-image";
+
+  let cover = document.createElement("div");
+  cover.appendChild(head);
+  cover.appendChild(image);
+
+  container.appendChild(cover);
+}
+
+async function getDinoName() {
+  const response = await fetch("api/dinoname");
+  const data = await response.json();
+
+  return data;
+}
+
+async function getDinoImage(name) {
+  const response = await fetch(`api/dinoimage?q=${name}`);
+  const data = await response.json().then((response) => response.thumbnailUrl);
+
+  return data;
+}
